acc302bf98bbc27208a8c8954c4339effe6f4dba
stephqwu <stephanie.wu@alumni.ubc.ca>
2024-12-13 02:57:30 -0800

    Fix tests

--- tests/test_read_zip.py
+++ tests/test_read_zip.py
@@ -15,17 +15,17 @@ if not os.path.exists('tests/test_zip_file'):
 with open('tests/test_zip_file/test.txt', 'w') as file:
     pass  # The 'pass' statement does nothing, creating an empty file
 
-test_files_txt_csv = ['test.txt', 'test.csv']
+test_files_txt_csv = ['test1.txt', 'test2.csv']
 test_files_txt_csv_nonempty = ['test1.txt', 'test2.csv', 'test.txt']
 # URL for cases (zip file containing 'test1.txt' and 'test2.csv')
-url_txt_csv_zip = 'https://github.com/UBC-MDS/heart_disease_predictor_py/raw/main/tests/files_txt_csv.zip
+url_txt_csv_zip = 'https://github.com/UBC-MDS/heart_disease_predictor_py/raw/main/tests/files_txt_csv.zip'
 
 # test read_zip function can download and extract a zip file containing two files of two different types 
 def test_read_zip_txt_csv():
-    read_zip(url_txt_csv_zip, 'tests/test_zip_data1')
+    read_zip(url_txt_csv_zip, 'tests/test_zip_empty')
     # List of files you expect to find in the directory
     for file in test_files_txt_csv:
-        file_path = os.path.join('tests/test_zip_data1', file)
+        file_path = os.path.join('tests/test_zip_empty', file)
         assert os.path.isfile(file_path)
     # clean up unzipped files
     for file in test_files_txt_csv:
@@ -35,10 +35,10 @@ def test_read_zip_txt_csv():
 # test read_zip function can download and extract a zip file containing two files of two different types
 # into a directory that already contains a file
 def test_read_zip_txt_csv_nonempty():
-    read_zip(url_txt_csv_zip, 'tests/test_zip_data2')
+    read_zip(url_txt_csv_zip, 'tests/test_zip_file')
     # List of files you expect to find in the directory
     for file in test_files_txt_csv_nonempty:
-        file_path = os.path.join('tests/test_zip_data2', file)
+        file_path = os.path.join('tests/test_zip_file', file)
         assert os.path.isfile(file_path)
     # clean up unzipped files
     for file in test_files_txt_csv:
